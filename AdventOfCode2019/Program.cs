216de547d39d0c5e5afb6f4e83aee90bedd6a17e
Ellis Valentiner <ellisvalentiner@gmail.com>
2019-11-03 10:15:07 -0500

    first commit

---
+++ AdventOfCode2019/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
