845bd3f71e2af9a1946bad36d7494a0458a975c9
Ellis Valentiner <ellisvalentiner@gmail.com>
2019-11-03 20:16:33 -0500

    Stub program for invoking each puzzle

--- AdventOfCode2019/Program.cs
+++ AdventOfCode2019/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Collections.Generic;
+using MarkdownLog;
 
 namespace AdventOfCode2019
 {
@@ -6,7 +10,20 @@ namespace AdventOfCode2019
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Puzzles puzzles = new Puzzles();
+            var methods = puzzles.GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly | BindingFlags.Static);
+
+            var data = new List<TimingRecord>();
+            foreach (var method in methods)
+            {
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                method.Invoke(puzzles, null);
+                stopWatch.Stop();
+                data.Add(new TimingRecord(method.Name, stopWatch.Elapsed.ToString("c")));
+            }
+            Console.WriteLine("# Advent of Code 2019");
+            Console.WriteLine(data.ToMarkdownTable());
         }
     }
 }
---
+++ AdventOfCode2019/Puzzles/Puzzles.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode2019
+{
+    public class TimingRecord
+    {
+        public TimingRecord(string puzzle, string elapsed)
+        {
+            Puzzle = puzzle;
+            Elapsed = elapsed;
+        }
+
+        public string Puzzle { get; set; }
+        public string Elapsed { get; set; }
+    }
+
+    public class Puzzles
+    {
+        public void Puzzle1()
+        {
+        }
+    }
+}
